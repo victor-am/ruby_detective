5893cfd9dd2a52908da4880b6e9af27e3bf2e848
Victor Marques <victor.atmorning@gmail.com>
2019-12-08 22:50:05 -0300

    Changes UI to show graph based on selection

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <div id="app">
-    <DependencyGraph :classesData="classesData"/>
+    <div class="toolbar">
+      <div class="search-bar">
+        <input v-model="classSearchTerm">
+      </div>
+
+      <ul class="class-list">
+        <li class="class-item" v-for="classData in filteredClassesList" :key="classData.full_name" @click="selectedClass = classData.full_name">
+          {{ classData.full_name }}
+        </li>
+      </ul>
+    </div>
+    <DependencyGraph class="graph" :classesData="filteredClassesData"/>
   </div>
 </template>
 
 <script>
+import Fuse from 'fuse.js'
+
 import DependencyGraph from './components/DependencyGraph.vue'
 
 const CLASSES_DATA = require('./data.json')
@@ -16,18 +29,87 @@ export default {
   },
   data() {
     return {
-      classesData: CLASSES_DATA
+      classesData: CLASSES_DATA,
+      classSearchTerm: '',
+      selectedClass: ''
+    }
+  },
+  computed: {
+    fuzzySearcher() {
+      const options = {
+        keys: [
+          {
+            name: 'full_name',
+            weight: 1
+          }
+        ]
+      }
+
+      return new Fuse(this.classesData, options)
+    },
+
+    filteredClassesList() {
+      return this.classSearchTerm == '' ? this.classesData : this.fuzzySearcher.search(this.classSearchTerm)
+    },
+
+    filteredClassesData() {
+      const selectedClass = this.selectedClass
+
+      const classes = this.classesData.filter((c) => {
+        return c.name == selectedClass || c.dependencies.includes(selectedClass)
+      })
+
+      const classesNames = classes.map((c) => c.full_name)
+
+      const matchedClassDependencies = this.classesData.filter((c) => classesNames.includes(c.full_name))
+      const uniqueDependencies = matchedClassDependencies.filter((cd) => !classesNames.includes(cd.full_name))
+
+      return classes.concat(uniqueDependencies)
     }
   }
 }
 </script>
 
 <style>
+/*! minireset.css v0.0.6 | MIT License | github.com/jgthms/minireset.css */html,body,p,ol,ul,li,dl,dt,dd,blockquote,figure,fieldset,legend,textarea,pre,iframe,hr,h1,h2,h3,h4,h5,h6{margin:0;padding:0}h1,h2,h3,h4,h5,h6{font-size:100%;font-weight:normal}ul{list-style:none}button,input,select,textarea{margin:0}html{box-sizing:border-box}*,*::before,*::after{box-sizing:inherit}img,video{height:auto;max-width:100%}iframe{border:0}table{border-collapse:collapse;border-spacing:0}td,th{padding:0}td:not([align]),th:not([align]){text-align:left}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+}
+
+.toolbar {
+  width: 25%;
+  background: #333;
+  color: #eee;
+  height: 100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+}
+
+.toolbar .search-bar {
+  width: 100%;
+  margin-bottom: 2px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  border-bottom: 1px solid #666;
+}
+
+.toolbar .class-list {
+  overflow-y: scroll;
+  height: auto;
+  text-align: left;
+  margin-left: 10px;
+}
+
+.graph {
+  width: 75%;
 }
 </style>
--- client/src/components/DependencyGraph.vue
+++ client/src/components/DependencyGraph.vue
@@ -17,12 +17,12 @@ const GRAPH_OPTIONS = {
     shape: "dot",
     scaling: {
       min: 10,
-      max: 100,
+      max: 50,
       label: {
-        min: 8,
+        min: 30,
         max: 30,
-        drawThreshold: 12,
-        maxVisible: 200
+        drawThreshold: 0,
+        maxVisible: 0
       }
     },
     font: {
@@ -61,6 +61,12 @@ export default {
     this.buildGraph()
   },
 
+  watch: {
+    classesData() {
+      this.buildGraph()
+    }
+  },
+
   computed: {
     dependencyGraphData() {
       const nodes = this.classesData.map((klass) => {
@@ -68,6 +74,7 @@ export default {
         return {
           id: klass.full_name,
           label: klass.full_name,
+          title: klass.full_name,
           value: klass.lines_of_code,
           group: klass.namespace
         }
@@ -96,83 +103,6 @@ export default {
 
       this.nodesDataset = nodes
       this.graph = new vis.Network(container, data, GRAPH_OPTIONS);
-      this.graph.on("click", this.neighbourhoodHighlight);
-    },
-
-    neighbourhoodHighlight(params) {
-      // if something is selected:
-      if (params.nodes.length > 0) {
-        this.highlightActive = true;
-        var i, j;
-        var selectedNode = params.nodes[0];
-        var degrees = 2;
-
-        // mark all nodes as hard to read.
-        for (var nodeId in this.allNodes) {
-          this.allNodes[nodeId].color = "rgba(200,200,200,0.5)";
-          if (this.allNodes[nodeId].hiddenLabel === undefined) {
-            this.allNodes[nodeId].hiddenLabel = this.allNodes[nodeId].label;
-            this.allNodes[nodeId].label = undefined;
-          }
-        }
-        var connectedNodes = this.graph.getConnectedNodes(selectedNode);
-        var allConnectedNodes = [];
-
-        // get the second degree nodes
-        for (i = 1; i < degrees; i++) {
-          for (j = 0; j < connectedNodes.length; j++) {
-            allConnectedNodes = allConnectedNodes.concat(
-              this.graph.getConnectedNodes(connectedNodes[j])
-            );
-          }
-        }
-
-        // all second degree nodes get a different color and their label back
-        for (i = 0; i < allConnectedNodes.length; i++) {
-          this.allNodes[allConnectedNodes[i]].color = "rgba(150,150,150,0.75)";
-          if (this.allNodes[allConnectedNodes[i]].hiddenLabel !== undefined) {
-            this.allNodes[allConnectedNodes[i]].label =
-              this.allNodes[allConnectedNodes[i]].hiddenLabel;
-            this.allNodes[allConnectedNodes[i]].hiddenLabel = undefined;
-          }
-        }
-
-        // all first degree nodes get their own color and their label back
-        for (i = 0; i < connectedNodes.length; i++) {
-          this.allNodes[connectedNodes[i]].color = undefined;
-          if (this.allNodes[connectedNodes[i]].hiddenLabel !== undefined) {
-            this.allNodes[connectedNodes[i]].label =
-              this.allNodes[connectedNodes[i]].hiddenLabel;
-            this.allNodes[connectedNodes[i]].hiddenLabel = undefined;
-          }
-        }
-
-        // the main node gets its own color and its label back.
-        this.allNodes[selectedNode].color = undefined;
-        if (this.allNodes[selectedNode].hiddenLabel !== undefined) {
-          this.allNodes[selectedNode].label = this.allNodes[selectedNode].hiddenLabel;
-          this.allNodes[selectedNode].hiddenLabel = undefined;
-        }
-      } else if (this.highlightActive === true) {
-        // reset all nodes
-        for (var nodeId2 in this.allNodes) {
-          this.allNodes[nodeId2].color = undefined;
-          if (this.allNodes[nodeId2].hiddenLabel !== undefined) {
-            this.allNodes[nodeId2].label = this.allNodes[nodeId2].hiddenLabel;
-            this.allNodes[nodeId2].hiddenLabel = undefined;
-          }
-        }
-        this.highlightActive = false;
-      }
-
-      // transform the object into an array
-      var updateArray = [];
-      for (nodeId in this.allNodes) {
-        if (this.allNodes.hasOwnProperty(nodeId)) {
-          updateArray.push(this.allNodes[nodeId]);
-        }
-      }
-      this.nodesDataset.update(updateArray);
     }
   }
 }
@@ -180,7 +110,7 @@ export default {
 
 <style scoped>
 #dependency-graph {
-  width: 1024px;
-  height: 768px;
+  width: 100%;
+  height: 100%;
 }
 </style>
