d2ca0203541f4a3f4430c6d89f8244a3b2f27e99
Victor Marques <victor.atmorning@gmail.com>
2019-12-09 11:51:00 -0300

    Add element UI components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,119 +1,36 @@
 <template>
   <div id="app">
-    <div class="toolbar">
-      <div class="search-bar">
-        <input v-model="classSearchTerm">
-        <button @click="showFullGraph = true; selectedClass = false">Show full project graph</button>
-      </div>
-
-      <ul class="class-list">
-        <li class="class-item" v-for="classData in filteredClassesList" :key="classData.full_name" @click="selectedClass = classData.full_name; showFullGraph = false">
-          {{ classData.full_name }}
-        </li>
-      </ul>
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
     </div>
-    <DependencyGraph class="graph" :classesData="filteredClassesData"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import Fuse from 'fuse.js'
-
-import DependencyGraph from './components/DependencyGraph.vue'
-
-const CLASSES_DATA = require('./data.json')
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    DependencyGraph
-  },
-  data() {
-    return {
-      classesData: CLASSES_DATA,
-      classSearchTerm: '',
-      selectedClass: '',
-      showFullGraph: false
-    }
-  },
-  computed: {
-    fuzzySearcher() {
-      const options = {
-        keys: [
-          {
-            name: 'full_name',
-            weight: 1
-          }
-        ]
-      }
-
-      return new Fuse(this.classesData, options)
-    },
-
-    filteredClassesList() {
-      return this.classSearchTerm == '' ? this.classesData : this.fuzzySearcher.search(this.classSearchTerm)
-    },
-
-    filteredClassesData() {
-      if (this.showFullGraph == true) { return this.classesData }
-
-      const selectedClass = this.selectedClass
-
-      const classes = this.classesData.filter((c) => {
-        return c.name == selectedClass || c.dependencies.includes(selectedClass)
-      })
-
-      const classesNames = classes.map((c) => c.full_name)
-
-      const matchedClassDependencies = this.classesData.filter((c) => classesNames.includes(c.full_name))
-      const uniqueDependencies = matchedClassDependencies.filter((cd) => !classesNames.includes(cd.full_name))
-
-      return classes.concat(uniqueDependencies)
-    }
+    HelloWorld
   }
 }
 </script>
 
 <style>
-/*! minireset.css v0.0.6 | MIT License | github.com/jgthms/minireset.css */html,body,p,ol,ul,li,dl,dt,dd,blockquote,figure,fieldset,legend,textarea,pre,iframe,hr,h1,h2,h3,h4,h5,h6{margin:0;padding:0}h1,h2,h3,h4,h5,h6{font-size:100%;font-weight:normal}ul{list-style:none}button,input,select,textarea{margin:0}html{box-sizing:border-box}*,*::before,*::after{box-sizing:inherit}img,video{height:auto;max-width:100%}iframe{border:0}table{border-collapse:collapse;border-spacing:0}td,th{padding:0}td:not([align]),th:not([align]){text-align:left}
-
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-}
-
-.toolbar {
-  width: 25%;
-  background: #333;
-  color: #eee;
-  height: 100%;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-}
-
-.toolbar .search-bar {
-  width: 100%;
-  margin-bottom: 2px;
-  padding-top: 20px;
-  padding-bottom: 20px;
-  border-bottom: 1px solid #666;
-}
-
-.toolbar .class-list {
-  overflow-y: scroll;
-  height: auto;
-  text-align: left;
-  margin-left: 10px;
-}
-
-.graph {
-  width: 75%;
+  margin-top: 60px;
 }
 </style>
